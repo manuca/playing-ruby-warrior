3b55c09a1330c2a3bdfb4c03cc22f2a1f4beefdf
Manuel Barros Reyes <manuca@gmail.com>
2014-02-19 11:13:28 -0300

    Level 6 - Basic version

--- player.rb
+++ player.rb
@@ -1,5 +1,14 @@
+class Direction
+  def self.oposite(direction)
+    (direction == :forward)? :backward : :forward
+  end
+end
+
 class Player
-  attr_accessor :health
+  attr_accessor :health, :direction
+  MAX_HEALTH    = 20
+  ATTACK_HEALTH = 12
+  # MIN_ARCHER_ATTACK_HEALTH =
 
   def receiving_damage?(warrior)
     if !health.nil? && health > warrior.health
@@ -9,16 +18,49 @@ class Player
     end
   end
 
-  def play_turn(warrior)
-    if warrior.feel.enemy?
-      warrior.attack!
-    elsif warrior.feel.captive?
-      warrior.rescue!
+  def set_direction!(warrior)
+    self.direction = :backward if direction.nil?
+    self.direction = :forward  if warrior.feel(direction).wall?
+  end
+
+  def needs_rest?(warrior)
+    warrior.health < MAX_HEALTH && !receiving_damage?(warrior)
+  end
+
+  def attack_or_retreat_strategy(warrior)
+    if warrior.health < ATTACK_HEALTH
+      retreat_strategy(warrior)
     else
-      if warrior.health <= 12 && !receiving_damage?(warrior)
+      attack_strategy(warrior)
+    end
+  end
+
+  def attack_strategy(warrior)
+    if warrior.feel(direction).empty?
+      warrior.walk!(direction)
+    elsif warrior.feel(direction).enemy?
+      warrior.attack!(direction)
+    end
+  end
+
+  def retreat_strategy(warrior)
+    warrior.walk!(Direction.oposite(direction))
+  end
+
+  def play_turn(warrior)
+    set_direction!(warrior)
+
+    if warrior.feel(direction).enemy?
+      warrior.attack!(direction)
+    elsif warrior.feel(direction).captive?
+      warrior.rescue!(direction)
+    else
+      if needs_rest?(warrior)
         warrior.rest!
+      elsif receiving_damage?(warrior)
+        attack_or_retreat_strategy(warrior)
       else
-        warrior.walk!
+        warrior.walk!(direction)
       end
     end
 
