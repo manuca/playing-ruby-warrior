84fe0280eb0daff6f8eabbcc8e89cfef9c914576
Manuel Barros Reyes <manuca@gmail.com>
2014-02-18 23:55:16 -0300

    Level 6

--- player.rb
+++ player.rb
@@ -2,16 +2,18 @@ class Player
   attr_accessor :health
 
   def receiving_damage?(warrior)
-   if !health.nil? && health > warrior.health
+    if !health.nil? && health > warrior.health
       true
     else
       false
     end
   end
-  
+
   def play_turn(warrior)
     if warrior.feel.enemy?
       warrior.attack!
+    elsif warrior.feel.captive?
+      warrior.rescue!
     else
       if warrior.health <= 12 && !receiving_damage?(warrior)
         warrior.rest!
@@ -19,7 +21,7 @@ class Player
         warrior.walk!
       end
     end
-    
+
     self.health = warrior.health
   end
 end
